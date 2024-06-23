4d89f7c9001f338716dec866aa1d44d95af30b02
RobinSardja <robinsardja@gmail.com>
2024-06-23 07:32:44 -0400

    Moved main app to eyedance.dart.

---
+++ lib/eyedance.dart
@@ -0,0 +1,21 @@
+import "package:flutter/material.dart";
+
+class EyeDance extends StatefulWidget {
+    const EyeDance({super.key});
+
+    @override
+    State<EyeDance> createState() => _EyeDanceState();
+}
+
+class _EyeDanceState extends State<EyeDance> {
+    @override
+    Widget build(BuildContext context) {
+        return const MaterialApp(
+            home: Scaffold(
+            body: Center(
+                child: Text( "EyeDance" )
+                )
+            )
+        );
+    }
+}
